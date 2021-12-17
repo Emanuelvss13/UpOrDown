d16e20be8198865b203024bc9cdc294527af808b
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-12-16 22:01:30 -0300

    mod: app.tsx with authContext and header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './styles/global.scss'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
+import Header from "./components/Header"
+import AuthContextProvider from './Context/Auth';
+import {Toaster} from "react-hot-toast"
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
+      <Toaster/>
+      <AuthContextProvider>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </AuthContextProvider>
     </Router>
   )
 }
