3f1ea2b7459b11d1ae1168ebb766e6fec4c648c1
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-12-18 14:46:25 -0300

    mod: refactor methods

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,9 @@
 import style from "./style.module.scss"
-import { collection, getDocs, addDoc, updateDoc, DocumentData, doc, arrayUnion, arrayRemove } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
-import { db } from "../../Config/firebase";
-import {toast} from "react-hot-toast"
 import { AuthContext } from "../../Context/Auth";
 import {ReactComponent as Up} from "../../Assets/up.svg"
 import {ReactComponent as Down} from "../../Assets/down.svg"
+import { FirestoreContext } from "../../Context/Firestore";
 
 interface Author {
   name: string;
@@ -34,78 +32,11 @@ export default function Home() {
 
   const [posts, setPosts] = useState<Post[]>([])
 
-  async function getPosts(){
-
-    const query = await getDocs(collection(db, "Posts"));
-
-    const postsRaw = query.docs.map(doc => ({ data: doc.data(), id: doc.id })) as Post[]
-
-    setPosts(postsRaw)
-  }
-
-  async function createPost(){
-
-    if(!user){
-      toast.error("Se registre para criar um tópico")
-      return 
-    }
-
-    try {
-
-      toast.loading("Postando...")
-
-      await addDoc(collection(db, "Posts"), {
-        author:{
-          id: user?.id,
-          name: user?.name,
-          photo: user?.avatar           
-        },
-        body: postBody,
-        end: false,
-        likes: [],
-        dislikes: [],
-        timestamp: new Date().toISOString(),
-        comentarios: []
-      }).then(() => {
-        toast.dismiss()
-        getPosts()
-      })
-
-      toast.success("Post criado com sucesso!")
-    } catch (error: any) {
-      toast.dismiss()
-      toast.error(error.message)
-    }
-  }
-
-  async function likePost(userId: string | undefined, postId: string){
-
-    if(!userId){
-      toast.error("Se registre para dar up nesse post")
-    }
-
-    await updateDoc(doc(db, "Posts", postId), {
-      likes: arrayUnion(userId),
-      dislikes: arrayRemove(userId)
-    }).then(() => getPosts())
-  }
-
-  async function dislikePost(userId: string | undefined, postId: string){
-
-    if(!userId){
-      toast.error("Se registre para dar down nesse post")
-    }
-
-    console.log(postId, userId)
-    await updateDoc(doc(db, "Posts", postId), {
-      likes: arrayRemove(userId),
-      dislikes: arrayUnion(userId)
-    }).then(() => getPosts())
-  }
+  const {createPost, allPosts, likePost, dislikePost} = useContext(FirestoreContext)
 
   useEffect(() => {
-    getPosts()
-  }, [])
+    setPosts(allPosts)
+  }, [allPosts])
 
   return (
     <div className={style.Container}>
@@ -116,14 +47,13 @@ export default function Home() {
           placeholder="Escreva um topico!"
           rows={5}
         />
-        <button onClick={createPost} >Postar</button>
+        <button onClick={() => createPost(postBody)} >Postar</button>
       </div>
 
       <div className={style.PostContentContainer} >
         {posts.length > 0 ?
           posts.map(post => (
             <div className={style.PostContainer} key={post.id}>
-              {console.log(post.id)}
               <div className={style.header} >
                 <div>
                   <img src={post.data.author.photo} alt="foto do autor" />
@@ -134,7 +64,7 @@ export default function Home() {
               </div>
               <div className={style.content} >
                 <div className={style.postBody} >
-                  <p onClick={() => console.log(post.id)} >{post.data.body}</p>
+                  <p>{post.data.body}</p>
                 </div>
                 <div className={style.actions} >
                   <div>
