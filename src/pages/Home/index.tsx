81600a7d1e0dd17e97883f28ad132ddcba988738
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-12-18 17:26:57 -0300

    mod: refactor post content in home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,8 @@
-import style from "./style.module.scss"
+import style from "./style.module.scss";
 import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../Context/Auth";
-import {ReactComponent as Up} from "../../Assets/up.svg"
-import {ReactComponent as Down} from "../../Assets/down.svg"
 import { FirestoreContext } from "../../Context/Firestore";
+import PostContent from "../../components/Post";
+import { useNavigate } from "react-router-dom";
 
 interface Author {
   name: string;
@@ -16,72 +15,47 @@ interface PostProps {
   end: boolean;
   likes: string[];
   dislikes: string[];
-  timestamp: string; 
+  timestamp: string;
 }
 
 interface Post {
-  data: PostProps
+  data: PostProps;
   id: string;
 }
 
 export default function Home() {
+  const [postBody, setPostBody] = useState("");
 
-  const {user} = useContext(AuthContext)
+  const [posts, setPosts] = useState<Post[]>([]);
 
-  const [postBody, setPostBody] = useState("")
-
-  const [posts, setPosts] = useState<Post[]>([])
-
-  const {createPost, allPosts, likePost, dislikePost} = useContext(FirestoreContext)
+  const { createPost, allPosts } = useContext(FirestoreContext);
 
   useEffect(() => {
-    setPosts(allPosts)
-  }, [allPosts])
+    setPosts(allPosts);
+  }, [allPosts]);
 
   return (
     <div className={style.Container}>
-      <div className={style.CreatePostContainer} >
+      <div className={style.CreatePostContainer}>
         <textarea
-          onChange={({target}) => setPostBody(target.value)}
+          onChange={({ target }) => setPostBody(target.value)}
           value={postBody}
           placeholder="Escreva um topico!"
           rows={5}
         />
-        <button onClick={() => createPost(postBody)} >Postar</button>
+        <button onClick={() => createPost(postBody)}>Postar</button>
       </div>
 
-      <div className={style.PostContentContainer} >
-        {posts.length > 0 ?
-          posts.map(post => (
-            <div className={style.PostContainer} key={post.id}>
-              <div className={style.header} >
-                <div>
-                  <img src={post.data.author.photo} alt="foto do autor" />
-                  <p>{post.data.author.name}</p>
-                </div>
-                <p >{new Date(post.data.timestamp).toLocaleDateString('pt-BR', {timeZone: 'UTC'})}</p>
-                {post.data.end ? <p className={style.off} >Encerrado</p> : <p className={style.on} >Ativo</p>}
-              </div>
-              <div className={style.content} >
-                <div className={style.postBody} >
-                  <p>{post.data.body}</p>
-                </div>
-                <div className={style.actions} >
-                  <div>
-                    <Up className={style.up} onClick={() => likePost(user?.id, post.id)} />
-                    <p>{post.data.likes.length}</p>
-                  </div>
-                  <div>
-                    <Down className={style.down} onClick={() => dislikePost(user?.id, post?.id)} />
-                    <p>{post.data.dislikes.length}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )) 
-        : 
-        "Nenhum pos ainda :("}  
+      <div className={style.PostContentContainer}>
+        {posts.length > 0
+          ? posts.map((post) => (
+              <PostContent
+                post={post}
+                key={post.id}
+              />
+            ))
+          : "Nenhum pos ainda :("}
       </div>
     </div>
-  )
+  );
 }
